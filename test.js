95f763c98f2ab4da0ab4887d8cc10aed61e297d4
Ashy6299 <adamsashraf80@gmail.com>
2023-02-22 08:36:04 +0000

    created second javascript file

---
+++ test.js
@@ -0,0 +1,17 @@
+var age = 10;
+
+console.log(age);
+
+let TheAge = 17;
+
+console.log(TheAge);
+
+console.log(15 * 15);
+console.log(25 / 5);
+console.log(parseInt("20") + 10);
+console.log(45 + 2 - 8);
+console.log(Math.random());
+console.log(Math.PI);
+console.log("15" + 8 + 4);
+
+
